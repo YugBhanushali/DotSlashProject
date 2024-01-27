9fd940350c38a3234629ae7d3eb39c31e2be2334
YugBhanushali <yughbhanushali241103@gmail.com>
2024-01-28 02:20:04 +0530

    add node

--- src/Pages/Tree.jsx
+++ src/Pages/Tree.jsx
@@ -1,161 +1,224 @@
-import React, { useCallback } from 'react';
-import './Tree.css';
-import 'reactflow/dist/style.css';
-import ELK from 'elkjs/lib/elk.bundled.js';
+import React, { useCallback, useEffect } from "react";
+import "./Tree.css";
+import "reactflow/dist/style.css";
+import ELK from "elkjs/lib/elk.bundled.js";
 
-import CNode from '../components/CNode';
-
+import CNode from "../components/CNode";
 
 import ReactFlow, {
-    MiniMap,
-    Controls,
-    Background,
-    useNodesState,
-    useEdgesState,
-    addEdge,
-    ReactFlowProvider,
-    Panel,
-    useReactFlow,
-  } from 'reactflow'; 
-import { nanoid } from 'nanoid';
-  
-  const tree = {
-    title: "",
-    nodes: [
-      {
-        id: "1",
-        position: { x: 0, y: 0 },
-        data: { value: "Root", title: "dljsf",img:"https://github.com/shadcn.png", likes: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. .", time: "" },
-        targetPosition: "right",
-        type: "custom",
-      },
-      {
-        id: "2",
-        position: { x: 0, y: 0 },
-        data: { value: "Root", title: "dljsf",img:"https://github.com/shadcn.png", likes: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", time: "" },
-        targetPosition: "right",
-        type: "custom",
-      },
-      {
-        id: "3",
-        position: { x: 0, y: 0 },
-        data: { value: "Root", title: "dljsf",img:"https://github.com/shadcn.png", likes: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", time: "" },
-        targetPosition: "right",
-        type: "custom",
-      },
-      {
-        id: "4",
-        position: { x: 0, y: 0 },
-        data: { value: "Root", title: "dljsf",img:"https://github.com/shadcn.png", likes: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", time: "" },
-        targetPosition: "right",
-        type: "custom",
-      },
-      {
-        id: "5",
-        position: { x: 0, y: 0 },
-        data: { value: "Root", title: "dljsf", likes: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", time: "" },
-        targetPosition: "right",
-        type: "custom",
-      },
-    ],
-    edges: [
-      { id: nanoid(5), source: "1", target: "2" },
-      { id: nanoid(5), source: "1", target: "3" },
-      { id: nanoid(5), source: "2", target: "4" },
-      { id: nanoid(5), source: "2", target: "5" },
-    ],
-  };
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  ReactFlowProvider,
+  Panel,
+  useReactFlow,
+} from "reactflow";
+import { nanoid } from "nanoid";
 
-const initialNodes = tree.nodes
+const tree = {
+  title: "",
+  nodes: [
+    {
+      id: "1",
+      position: { x: 0, y: 0 },
+      data: {
+        value: "Root",
+        title: "dljsf",
+        img: "https://github.com/shadcn.png",
+        likes: 0,
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. .",
+        time: "",
+      },
+      targetPosition: "right",
+      type: "custom",
+    },
+    {
+      id: "2",
+      position: { x: 0, y: 0 },
+      data: {
+        value: "Root",
+        title: "dljsf",
+        img: "https://github.com/shadcn.png",
+        likes: 0,
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        time: "",
+      },
+      targetPosition: "right",
+      type: "custom",
+    },
+    {
+      id: "3",
+      position: { x: 0, y: 0 },
+      data: {
+        value: "Root",
+        title: "dljsf",
+        img: "https://github.com/shadcn.png",
+        likes: 0,
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        time: "",
+      },
+      targetPosition: "right",
+      type: "custom",
+    },
+    {
+      id: "4",
+      position: { x: 0, y: 0 },
+      data: {
+        value: "Root",
+        title: "dljsf",
+        img: "https://github.com/shadcn.png",
+        likes: 0,
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        time: "",
+      },
+      targetPosition: "right",
+      type: "custom",
+    },
+    {
+      id: "5",
+      position: { x: 0, y: 0 },
+      data: {
+        value: "Root",
+        title: "dljsf",
+        img: "https://github.com/shadcn.png",
+        likes: 0,
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        time: "",
+      },
+      targetPosition: "right",
+      type: "custom",
+    },
+  ],
+  edges: [
+    { id: nanoid(5), source: "1", target: "2" },
+    { id: nanoid(5), source: "1", target: "3" },
+    { id: nanoid(5), source: "2", target: "4" },
+    { id: nanoid(5), source: "2", target: "5" },
+  ],
+};
 
-const nodeTypes = {custom:CNode}  
-  
+const initialNodes = tree.nodes;
+
+const nodeTypes = { custom: CNode };
+
 const initialEdges = tree.edges;
 
-const elk = new ELK(); 
+const elk = new ELK();
 
 const useLayoutedElements = () => {
-    const { getNodes, setNodes, getEdges, fitView } = useReactFlow();
-    const defaultOptions = {
-      'elk.algorithm': 'layered',
-      'elk.direction': 'RIGHT',
-      'elk.layered.spacing.nodeNodeBetweenLayers': 100,
-      'elk.spacing.nodeNode': 80,
+  const { getNodes, setNodes, getEdges, fitView } = useReactFlow();
+  const defaultOptions = {
+    "elk.algorithm": "layered",
+    "elk.direction": "RIGHT",
+    "elk.layered.spacing.nodeNodeBetweenLayers": 100,
+    "elk.spacing.nodeNode": 80,
+  };
+
+  const getLayoutedElements = useCallback((options) => {
+    const layoutOptions = { ...defaultOptions, ...options };
+    const graph = {
+      id: "root",
+      layoutOptions: layoutOptions,
+      children: getNodes(),
+      edges: getEdges(),
     };
-  
-    const getLayoutedElements = useCallback((options) => {
-      const layoutOptions = { ...defaultOptions, ...options };
-      const graph = {
-        id: 'root',
-        layoutOptions: layoutOptions,
-        children: getNodes(),
-        edges: getEdges(),
-      };
-  
-      elk.layout(graph).then(({ children }) => {
-        // By mutating the children in-place we saves ourselves from creating a
-        // needless copy of the nodes array.
-        children.forEach((node) => {
-          node.position = { x: node.x, y: node.y };
-        });
-  
-        setNodes(children);
-        window.requestAnimationFrame(() => {
-          fitView();
-        });
+
+    elk.layout(graph).then(({ children }) => {
+      // By mutating the children in-place we saves ourselves from creating a
+      // needless copy of the nodes array.
+      children.forEach((node) => {
+        node.position = { x: node.x, y: node.y };
       });
-    }, []);
-  
-    return { getLayoutedElements };
-  };
-  
-  const LayoutFlow = () => {
-    const [nodes, , onNodesChange] = useNodesState(initialNodes);
-    const [edges, , onEdgesChange] = useEdgesState(initialEdges);
-    const { getLayoutedElements } = useLayoutedElements();
-  
-    return (
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        nodeTypes={nodeTypes}
-        fitView
+
+      setNodes(children);
+      window.requestAnimationFrame(() => {
+        fitView();
+      });
+    });
+  }, []);
+
+  return { getLayoutedElements };
+};
+
+const LayoutFlow = () => {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, , onEdgesChange] = useEdgesState(initialEdges);
+  const { getLayoutedElements } = useLayoutedElements();
+
+  useEffect(() => {
+    getLayoutedElements({
+      "elk.algorithm": "layered",
+      "elk.direction": "RIGHT",
+    });
+  }, []);
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      nodeTypes={nodeTypes}
+      fitView
+    >
+      <Panel
+        position="top-right"
+        className="flex flex-col justify-center items-center gap-2"
       >
-        <Panel position="top-right" className='flex flex-col justify-center items-center gap-2'>
-          <button className="w-[80px] h-1/3 text-[12px] bg-[#ffa45c] text-white p-1 font-semibold rounded-xl mx-2"
-            onClick={() =>
-              getLayoutedElements({ 'elk.algorithm': 'layered', 'elk.direction': 'DOWN' })
-            }
-          >
-            vertical
-          </button>
-          <button className="w-[80px] h-1/3 text-[12px] bg-[#ffa45c] text-white p-1 font-semibold rounded-xl"
-            onClick={() =>
-              getLayoutedElements({ 'elk.algorithm': 'layered', 'elk.direction': 'RIGHT' })
-            }
-          >
-            horizontal
-          </button>
-        </Panel>
-        <Background variant="dots" color="#fff" gap={12} size={1} className="bg-[#282523]" />
-      </ReactFlow>
-    );
-  };
-   
+        <button
+          className="w-[80px] h-1/3 text-[12px] bg-[#ffa45c] text-white p-1 font-semibold rounded-xl mx-2"
+          onClick={() =>
+            getLayoutedElements({
+              "elk.algorithm": "layered",
+              "elk.direction": "DOWN",
+            })
+          }
+        >
+          vertical
+        </button>
+        <button
+          className="w-[80px] h-1/3 text-[12px] bg-[#ffa45c] text-white p-1 font-semibold rounded-xl"
+          onClick={() =>
+            getLayoutedElements({
+              "elk.algorithm": "layered",
+              "elk.direction": "RIGHT",
+            })
+          }
+        >
+          horizontal
+        </button>
+      </Panel>
+      <Background
+        variant="dots"
+        color="#fff"
+        gap={12}
+        size={1}
+        className="bg-[#282523]"
+      />
+    </ReactFlow>
+  );
+};
+
 export default function Tree() {
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-   
-    const onConnect = useCallback(
-      (params) => setEdges((eds) => addEdge(params, eds)),
-      [setEdges],
-    );
-   
-    return (
-      <div style={{ width: '100vw', height: '100vh' }}>
-        {/* <ReactFlow
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      {/* <ReactFlow
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
@@ -166,9 +229,9 @@ export default function Tree() {
           <MiniMap nodeColor='#ff0072' nodeStrokeWidth={3} zoomable pannable />
           <Background variant="dots" color="#000" gap={12} size={1} className="bg-black" />
         </ReactFlow> */}
-        <ReactFlowProvider>
-            <LayoutFlow />
-        </ReactFlowProvider>
-      </div>
-    );
-  }
+      <ReactFlowProvider>
+        <LayoutFlow />
+      </ReactFlowProvider>
+    </div>
+  );
+}
--- src/components/CNode.jsx
+++ src/components/CNode.jsx
@@ -1,26 +1,46 @@
-import { useCallback } from 'react';
-import { Handle, Position } from 'reactflow';
+import { useCallback } from "react";
+import { Handle, Position } from "reactflow";
+import { IoIosAddCircleOutline,IoIosHeartEmpty } from "react-icons/io";
 
 const handleStyle = { left: 10 };
- 
+
 function CustomNode({ data, isConnectable }) {
   const onChange = useCallback((evt) => {
     console.log(evt.target.value);
   }, []);
 
   return (
-    <div className="node flex flex-col relative min-w-[250px] h-[250px] max-h-[650px] rounded-lg nodrag border border-[#3a3a3a] shadow-md">
-      <Handle type="target" position={Position.Left} isConnectable={isConnectable} />
-      <div className='bg-[#020308] flex gap-2 text-white text-pretty text-[12px] py-1 px-3 rounded-t-lg'>
-      <div>
-        <img height={20} width={20} className=' rounded-full' src={data.img}/>
-      </div>
-      <div>
-        {data.title}
-      </div>
+    <div className="node flex flex-col relative min-w-[250px] h-[250px] max-h-[650px] rounded-xl nodrag border border-[#3a3a3a] shadow-md">
+      <Handle
+        type="target"
+        position={Position.Left}
+        isConnectable={isConnectable}
+      />
+      <div className="bg-[#020308] flex gap-2 text-white text-pretty text-[12px] py-1 px-3 rounded-lg">
+        <div>
+          <img
+            height={20}
+            width={20}
+            className=" rounded-full"
+            src={data.img}
+          />
+        </div>
+        <div>{data.title}</div>
       </div>
       <div className="bg-[#020308] text-white text-pretty text-sm h-full p-3 text-[8px] rounded-b-lg overflow-y-scroll border border-[#3a3a3a]">
         {data?.content}
+      </div>
+      <div className="bg-[#020308] flex justify-end items-center gap-2 rounded-lg py-2">
+        <div>
+          <button className="flex text-white justify-center items-center text-[13px] gap-1">
+            <IoIosHeartEmpty color="white"/> <span className=" text-[10px]">{data?.likes}</span>
+          </button>
+        </div>
+        <div className="mr-2">
+          <button className="flex text-[13px]">
+            <IoIosAddCircleOutline color="white"/>
+          </button>
+        </div>
       </div>
       {/* <Handle
         type="source"
@@ -29,13 +49,18 @@ function CustomNode({ data, isConnectable }) {
         style={handleStyle}
         isConnectable={isConnectable}
       /> */}
-      <Handle type="source" position={Position.Right} id="b" isConnectable={isConnectable} />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="b"
+        isConnectable={isConnectable}
+      />
     </div>
   );
 }
- 
+
 export default CustomNode;
 
 // const nodeTypes = useMemo(() => ({ custom: CustomNode }), []);
- 
+
 // return <ReactFlow nodeTypes={nodeTypes} />;
